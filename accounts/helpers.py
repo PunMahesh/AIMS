cb8b6a83fca6dd82247180d621145b81d1cc4bc0
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2023-03-25 00:06:41 +0545

    updated accounts

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -1,9 +1,14 @@
 from django.core.mail import send_mail
 import uuid
+from django.conf import settings
 
 
-def send_forget_password(email):
-    token = str(uuid.uuid4())
+def send_forget_password(email,token):
+    
     subject = "Your forget Password Link"
-    message = f"Hi, Click on the link to reset your password"
+    message = f"Hi, Click on the link to reset your password http://127.0.0.1:8000/changepassword/{token}/"
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list)
+    return True
 
