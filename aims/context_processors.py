0bc15e783a0161d1b5906ea5d9ce48916580c76b
amritab3 <np03cs4a210058@heraldcollege.edu.np>
2023-04-07 00:46:09 +0545

    add base template and modify sidebar code

---
+++ aims/context_processors.py
@@ -0,0 +1,11 @@
+def sidebar_items(request):
+    return {
+        'sidebar_items': [
+            {'name': 'Dashboard', 'url': 'farmerHome', 'icon': 'house'},
+            {'name': 'Crops', 'url': 'crops', 'icon': 'psychiatry'},
+            {'name': 'Equipments', 'url': "404", 'icon': 'agriculture'},
+            {'name': 'Market', 'url': "404", 'icon': 'store'},
+            {'name': 'Worker', 'url': "404", 'icon': 'engineering'},
+            {'name': 'Feed', 'url': "404", 'icon': 'article'},
+        ]
+    }
