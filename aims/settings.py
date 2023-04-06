574714b07d2c125ad51bcaec60c0f57bad6833a9
amritab3 <np03cs4a210058@heraldcollege.edu.np>
2023-04-07 00:46:36 +0545

    register context processor in settings

--- aims/settings.py
+++ aims/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'aims.context_processors.sidebar_items'
             ],
         },
     },
@@ -82,7 +84,7 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':'3306',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'rootpass',
     }
 }
 
