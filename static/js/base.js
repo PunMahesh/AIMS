869bf1706436d0ed15ca100f59bee0ae4f3a19b6
amritab3 <np03cs4a210058@heraldcollege.edu.np>
2023-04-09 17:53:44 +0545

    fix styles and add js to close dropdown on window click

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,17 @@
 const nameContainer = document.querySelector('.name-container');
 
-nameContainer.addEventListener('click', () => {
+nameContainer.addEventListener('click', (event) => {
     const dropdownOptions = document.querySelector(".dropdown-options");
     dropdownOptions.classList.toggle("show");
+    event.stopPropagation();
 });
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = (event) => {
+    if (!event.target.matches('.dropdown-item')) {
+        const dropdownOptions = document.querySelector(".dropdown-options");
+        if (dropdownOptions.classList.contains('show')) {
+            dropdownOptions.classList.remove('show');
+        }
+    }
+}
