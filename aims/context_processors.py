b30a7986d65b5facd383f68dd36dfda3867b3e3c
amritab3 <np03cs4a210058@heraldcollege.edu.np>
2023-04-15 12:52:42 +0545

    fix path for equipments in sidebar item

--- aims/context_processors.py
+++ aims/context_processors.py
@@ -3,7 +3,7 @@ def sidebar_items(request):
         'sidebar_items': [
             {'name': 'Dashboard', 'url': 'farmerHome', 'icon': 'house'},
             {'name': 'Crops', 'url': 'crops', 'icon': 'psychiatry'},
-            {'name': 'Equipments', 'url': "404", 'icon': 'agriculture'},
+            {'name': 'Equipments', 'url': "equipments", 'icon': 'agriculture'},
             {'name': 'Market', 'url': "404", 'icon': 'store'},
             {'name': 'Worker', 'url': "404", 'icon': 'engineering'},
             {'name': 'Feed', 'url': "404", 'icon': 'article'},
