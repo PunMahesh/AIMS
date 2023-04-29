1e4766dc9ee6f50735e79384d44f70da6c3df1b3
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2023-04-29 23:43:45 +0545

    settings updated

--- aims/settings.py
+++ aims/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'farmer',
-    'equipments',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'aims.context_processors.sidebar_items',
             ],
         },
     },
