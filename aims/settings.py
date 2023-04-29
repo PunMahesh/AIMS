8f8237807e81e0aafbc78f4ff52dbc46d626b5a9
amritab3 <np03cs4a210058@heraldcollege.edu.np>
2023-04-30 00:55:52 +0545

    remove db password

--- aims/settings.py
+++ aims/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME':'aims',
         'USER':'root',
-        'PASSWORD':'rootpass',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306',
         'OPTIONS': {
