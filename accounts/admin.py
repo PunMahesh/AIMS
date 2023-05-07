2c0400ea57bb466f922e5140cad475930d406932
Rajat <sapkotarajat59@gmail.com>
2023-05-07 21:22:11 +0545

    arrow in contact us button

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from accounts.models import User
+from farmer.models import Farmer_KYC
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Farmer_KYC)
