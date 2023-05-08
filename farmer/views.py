eeb00580312568a24d6bdbdb8bdfb811386fcf47
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2023-05-09 01:14:23 +0545

    kyc error handling

--- farmer/views.py
+++ farmer/views.py
@@ -76,33 +76,6 @@ def kyc(request):
             FrontPic=FrontPic,
             BackPic=BackPic
         )
-        # context = {
-        #     'PassportPhoto':PassportPhoto,
-        #     'first_name':first_name,
-        #     'MiddleName':MiddleName,
-        #     'Last_name':Last_name,
-        #     'Gender':Gender,
-        #     'MaritualStatus':MaritualStatus,
-        #     'Dob':Dob,
-        #     'Nationality':Nationality,
-        #     'Citizenship':Citizenship,
-        #     'Passport':Passport,
-        #     'Residential':Residential,
-        #     'FatherName':FatherName,
-        #     'MotherName':MotherName,
-        #     'GrandfatherName':GrandfatherName,
-        #     'GrandMotherName':GrandMotherName,
-        #     'SpouseName':SpouseName,
-        #     'SonName':SonName,
-        #     'DaughterName':DaughterName,
-        #     'Country':Country,
-        #     'District':District,
-        #     'Province':Province,
-        #     'Municipality':Municipality,
-        #     'WardNo':WardNo,
-        #     'Street':Street
-        # }
-        # if User.is_customer == True:
         check_kyc.save()
         return render(request,'kyc_form.html')
     # Return an empty context dictionary for the GET request
@@ -119,91 +92,6 @@ def E_chart(request):
     context = {"equipment":equipment}
     print(equipment)
     return render(request,'farmer_home.html',context)
-
-
-# def my_pdf(request):
-
-#     #Retrieve the data from the model
-#     kyc = Farmer_KYC.objects.get(user = request.user.id)
-
-#     #Create a response object with the PDF MIME type
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="my_file.pdf"'
-
-#     #Create a PDF object and add the data to it
-#     pdf = canvas.Canvas(response)
-#     pdf.drawString(100, 100, str(kyc.first_name))
-#     pdf.drawString(100, 120, str(kyc.MiddleName))
-#     pdf.drawString(100, 140, str(kyc.Last_name))
-#     pdf.drawString(100, 160, str(kyc.Gender))
-#     pdf.drawString(100, 180, str(kyc.MaritualStatus))
-#     pdf.drawString(100, 200, str(kyc.Dob))
-#     pdf.drawString(100, 220, str(kyc.Nationality))
-#     pdf.drawString(100, 240, str(kyc.Citizenship))
-#     pdf.drawString(100, 260, str(kyc.Passport))
-#     pdf.drawString(100, 280, str(kyc.Residential))
-#     pdf.drawString(100, 300, str(kyc.FatherName))
-#     pdf.drawString(100, 320, str(kyc.MotherName))
-#     pdf.drawString(100, 340, str(kyc.GrandfatherName))
-#     pdf.drawString(100, 380, str(kyc.GrandMotherName))
-#     pdf.drawString(100, 400, str(kyc.SpouseName))
-#     pdf.drawString(100, 420, str(kyc.DaughterName))
-#     pdf.drawString(100, 440, str(kyc.Country))
-#     pdf.drawString(100, 460, str(kyc.Province))
-#     pdf.drawString(100, 480, str(kyc.District))
-#     pdf.drawString(100, 500, str(kyc.WardNo))
-#     pdf.drawString(100, 520, str(kyc.Street))
-#     pdf.drawString(100, 540, str(kyc.CitizenProof))
-
-#     field = getattr(kyc, 'PassportPhoto', None)
-#     if isinstance(field, ImageField) and field.name == 'PassportPhoto':
-#         image_path = os.path.join(settings.MEDIA_ROOT, field.name)
-#         img = ImageReader(image_path)
-#         pdf.drawImage(img, 100, y, width=2*inch, height=2*inch)
-#         y -= 100  # move down for the next image
-    
-#     if isinstance(field, ImageField) and field.name == 'FrontPic':
-#         image_path = os.path.join(settings.MEDIA_ROOT, field.name)
-#         img = ImageReader(image_path)
-#         pdf.drawImage(img, 100, y, width=2*inch, height=2*inch)
-#         y -= 100  # move down for the next image
-
-#     if isinstance(field, ImageField) and field.name == 'BackPic':
-#         image_path = os.path.join(settings.MEDIA_ROOT, field.name)
-#         img = ImageReader(image_path)
-#         pdf.drawImage(img, 300, y, width=2*inch, height=2*inch)
-#        y -= 100  # move down for the next image
-
-
-#     #Add more data to the PDF as needed
-
-#     #Close the PDF object and return the response
-#     pdf.showPage()
-#     pdf.save()
-#     return response
-
-# def generate_pdf(request):
-#     # Get the KYC form of the logged-in user
-#     kyc = Farmer_KYC.objects.get(user=request.user)
-    
-#     # Get the HTML template
-#     template = get_template('pdf.html')
-    
-#     # Render the template with the KYC data
-#     context = {'kyc': kyc, 'pdf_url': ''}
-#     print (context)
-#     html = template.render(context)
-    
-#     # Generate the PDF
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="my_pdf.pdf"'
-#     HTML(string=html).write_pdf(response)
-    
-#    # Set the PDF URL in the context
-#     context['pdf_url'] = response.get_signed_url(600)
-    
-#     # Return the template with the PDF URL
-#     return HttpResponse(html)
 
 def generate_pdf(request):
     user = request.user.id
