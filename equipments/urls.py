7c28a86f2e7cfaae9fe668f8ce87977e669a52d7
Rajat <sapkotarajat59@gmail.com>
2023-05-08 20:21:06 +0545

    market + equipment + product

--- equipments/urls.py
+++ equipments/urls.py
@@ -5,5 +5,6 @@ from equipments import views
 urlpatterns = [
     path("add_equipment", views.add_equipment, name='add_equipment'),
     path("equipments", views.view_equipments, name='equipments'),
-    path("equipments/<int:id>", views.get_equipment, name='get_equipment')
+    path("equipments/<int:id>", views.get_equipment, name='get_equipment'),
+    path("equipments-market", views.equipments_market, name='equipments-market'),
 ]
